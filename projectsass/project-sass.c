00d69551a60df953d9a5764ec7e349dbab52d946
unknown <bousilmustapha@gmail.com>
2022-09-21 10:49:39 +0100

    last version

--- projectsass/project-sass.c
+++ projectsass/project-sass.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+//Structure Save Product
 struct medication{
   int  Code;
   char Name[30];
@@ -8,94 +11,109 @@ struct medication{
   float Price;
 };
 struct medication T_product[100];
-
-
+/////////////////////////////////////////////////////////////
+//Structure save product sold
 struct Sold{
   char Name[20];
   int sold;
   float price;
-  int h,m,s;
-
+  int day,month,year;
 };
 struct Sold T_sold[100];
+////////////////////////////////////////////////////////////
+
+int j,i;
+
+
+void get_Data(int i,float price_TTC){
+   printf("##--Product Number %d--\n",i+1);
+   printf("--------------------------------------\n");
+   printf("Name Product      : %s \n"   ,T_product[i].Name);
+   printf("Quantity Product  : %d \n"   ,T_product[i].Quantity);
+   printf("Product Price     : %.2f$ \n",T_product[i].Price);
+   printf("Price(TTC)        : %.2f$ \n",price_TTC);
+   printf("------------------------------------\n");
+}
+
+
+
 int count_pro=0;
 void Add(){
-  int n,i;
-  printf("\t##--------------Add Product--------------##\n");
-  printf("Enter How Mush you want to add ");
+  int n;
+  printf("-----------------------------------------------\n");
+  printf("\##--------------Add Product-----------------##\n");
+  printf("-----------------------------------------------\n");
+  printf("//->Enter How Mush Product  you want to add ");
   scanf("%d",&n);
 
   for(i=1;i<=n;i++){
     printf("##---Product Number : %d \n",i);
-    printf("Code :");
+    printf("Code Product :");
     scanf("%d",&T_product[count_pro].Code);
-    printf("Name :");
+    printf("Name Product :");
     scanf("%s",&T_product[count_pro].Name);
     printf("Quantity :");
     scanf("%d",&T_product[count_pro].Quantity);
-    printf("Price :");
+    printf("Price($) :");
     scanf("%f",&T_product[count_pro].Price);
     count_pro++;
 }}
 void Display_Pro(){
-	    int i,n,j;
+    int choose;
+    int or_code , or_quantity , or_price;
+    char or_name[20];
     float price_TTC;
-    char c[100];int t;
+    printf("\t##-------------------------------------------##\n");
     printf("\t##--------------Display Product--------------##\n");
+    printf("\t##-------------------------------------------##\n");
     do{
      printf("Enter (2) Order By Alphabitic Or (1) Order By Price\n");
-     scanf("%d",&n);
-    }while(n!=2 && n!=1);
-    if(n==2){
-        int t;
-        int o_code , o_quantity;
-        //Order By Alphabitique
+     scanf("%d",&choose);
+    }while(choose!=2 && choose!=1);
+    if(choose==2){
+        //Order By Alphabitique Croissent
         for(i=0;i<count_pro;i++){
             for(j=i+1;j<count_pro;j++){
                 if(strcmp(T_product[i].Name,T_product[j].Name) > 0){
-                    strcpy(c,T_product[i].Name);
-                    t=T_product[i].Price;
-                    o_code=T_product[i].Code;
-                    o_quantity=T_product[i].Quantity;
+
+                    //Save first data in this variable
+                    strcpy(or_name,T_product[i].Name);
+                    or_price=T_product[i].Price;
+                    or_code=T_product[i].Code;
+                    or_quantity=T_product[i].Quantity;
+
+                    //switch element
 
                     strcpy(T_product[i].Name,T_product[j].Name);
                     T_product[i].Price=T_product[j].Price;
                     T_product[i].Quantity=T_product[j].Quantity;
                     T_product[i].Code=T_product[j].Code;
 
-                    strcpy(T_product[j].Name,c);
-                    T_product[j].Price=t;
-                    T_product[j].Quantity=o_quantity;
-                    T_product[j].Code=o_code;
+                    strcpy(T_product[j].Name,or_name);
+                    T_product[j].Price=or_price;
+                    T_product[j].Quantity=or_quantity;
+                    T_product[j].Code=or_code;
                 }
             }
         }
         for(i=0;i<count_pro;i++){
-                if(T_product[i].Quantity==0){
-                    continue;
-                }else{
-
-                 printf("##--Product Number \n",i);
-                 printf("Name : %s \n",T_product[i].Name);
-                 printf("Quantity : %d \n",T_product[i].Quantity);
-        printf("Price : %.2f$ \n",T_product[i].Price);
-        price_TTC=T_product[i].Price + 0.15*T_product[i].Price;
-        printf("Price(TTC) : %.2f$ \n",price_TTC);
-        printf("------------------------------------\n");}
-
+            if(T_product[i].Quantity==0){
+                continue;
+            }else{
+                price_TTC=T_product[i].Price + 0.15*T_product[i].Price;
+                get_Data(i,price_TTC);
+            }
         }
-
-    }else if(n==1){
-        char o_Name[20];
-        int o_code , o_quantity;
-        //Order By Price
+    }else if(choose==1){
+        //Order By Price decroissent
         for(int i=0;i<count_pro;i++){
             for(int j=i+1;j<count_pro;j++){
                 if(T_product[i].Price < T_product[j].Price){
-                    t=T_product[i].Price;
-                    strcpy(o_Name,T_product[i].Name);
-                    o_code=T_product[i].Code;
-                    o_quantity=T_product[i].Quantity;
+
+                    or_price=T_product[i].Price;
+                    strcpy(or_name,T_product[i].Name);
+                    or_code=T_product[i].Code;
+                    or_quantity=T_product[i].Quantity;
 
 
                     T_product[i].Price=T_product[j].Price;
@@ -103,10 +121,10 @@ void Display_Pro(){
                     T_product[i].Code=T_product[j].Code;
                     T_product[i].Quantity=T_product[j].Quantity;
 
-                    T_product[j].Price=t;
-                    strcpy(T_product[j].Name,o_Name);
-                    T_product[j].Quantity=o_quantity;
-                    T_product[j].Code=o_code;
+                    T_product[j].Price=or_price;
+                    strcpy(T_product[j].Name,or_name);
+                    T_product[j].Quantity=or_quantity;
+                    T_product[j].Code=or_code;
                 }
             }
         }
@@ -114,61 +132,51 @@ void Display_Pro(){
           if(T_product[i].Quantity==0){
             continue;
           }else{
-            printf("##--Product Number \n",i);
-            printf("Name : %s \n",T_product[i].Name);
-            printf("Quantity : %d \n",T_product[i].Quantity);
-            printf("Price : %.2f$ \n",T_product[i].Price);
-            price_TTC=T_product[i].Price + 0.15*T_product[i].Price;
-            printf("Price(TTC) : %.2f$ \n",price_TTC);
-            printf("------------------------------------\n");
+              price_TTC=T_product[i].Price + 0.15*T_product[i].Price;
+              get_Data(i,price_TTC);
           }
         }
      }
 
 }
 void Search_Pro(){
-int n,i,code,quantity;
+int choose,code,quantity;
   float price_TTC;
   do{
      printf("Search (1) by Code Or (2)  By Quantity\n");
-     scanf("%d",&n);
-    }while(n!=2 && n!=1);
-  if(n==1){
-     printf("Enter Code Product : ");
+     scanf("%d",&choose);
+    }while(choose!=2 && choose!=1);
+  if(choose==1){
+     printf("#-------search by Code------------#\n");
+     printf("#---------------------------------# \n");
+     printf("Code :");
      scanf("%d",&code);
      for(i=0;i<count_pro;i++){
         if(code==T_product[i].Code){
-           printf("Name : %s \n",T_product[i].Name);
-           printf("Quantity : %d \n",T_product[i].Quantity);
-           printf("Price : %.2f$\n",T_product[i].Price);
            price_TTC=T_product[i].Price + 0.15*T_product[i].Price;
-           printf("Price(TTC) : %.2f $ \n",price_TTC);
+           get_Data(i,price_TTC);
+           //count_err++;
         }
      }
-  }else if(n==2){
-     printf("\nsearch by quantity: ");
+  }else if(choose==2){
+     printf("\n #-------search by quantity------------#");
+     printf("#----------------------------------------# \n");
    scanf("%d",&quantity);
    for(i=0;i<count_pro;i++){
    if(quantity==T_product[i].Quantity){
-   printf("%s\n",T_product[i].Name);
-    printf("price of product: %.2f$ \n", T_product[i].Price);
-    price_TTC = T_product[i].Price + 0.15*T_product[i].Price;
-    printf("price (TTC) of product: %.2f$ \n", price_TTC);
-    printf("\n---------------------------------------\n");
-
+      price_TTC=T_product[i].Price + 0.15*T_product[i].Price;
+      get_Data(i,price_TTC);
     }
    }
   }
 }
 /////////////////////////////////////////////////////////////
 int count_sold=0;
-
 int code_edit;
-void Edit_Qty(){
 
- int h,m,s;
- int sold;
- int i;
+
+void Edit_Qty(){
+ int sold;//variable for sold product
  printf("Enter Code Product :");
  scanf("%d",&code_edit);
  printf("\n");
@@ -180,21 +188,21 @@ void Edit_Qty(){
       printf("New Quantity :");
       scanf("%d",&T_product[i].Quantity);
       if(qty_old > T_product[i].Quantity){
-        printf("Enter time (in HH:MM:SS)");
-        scanf("%02d:%02d:%02d",&h,&m,&s);
         sold=qty_old - T_product[i].Quantity;
-        //printf("sold : %d",sold);
-        //printf("COunt SOld %d",count_sold);
+        //Save data in table structur Sold;
+        time_t t;
+        time(&t);
+        struct tm *time=localtime(&t);
+        T_sold[count_sold].day=time->tm_mday;
+        T_sold[count_sold].month=time->tm_mon +1;
+        T_sold[count_sold].year=time->tm_year + 1900;
         T_sold[count_sold].sold=sold;
         strcpy(T_sold[count_sold].Name,T_product[i].Name);
         T_sold[count_sold].price=T_product[i].Price;
-        T_sold[count_sold].h=h;
-        T_sold[count_sold].m=m;
-        T_sold[count_sold].s=s;
         count_sold++;
       }
     }
- }
+  }
 }
 int old_quantity(){
     int old_qty;
@@ -208,6 +216,7 @@ int old_quantity(){
 /////////////////////////////////////////////////////////////////
 void Delete_Pro(){
     int code,index;
+    printf("Enter Code Product :");
     scanf("%d",&code);
     for(int i=0;i<count_pro;i++){
         if(code==T_product[i].Code){
@@ -216,50 +225,46 @@ void Delete_Pro(){
                 T_product[i]=T_product[i+1];
             }
             count_pro--;
+            printf("##--SUccess Delete This Product ! ##");
+            break;
         }
+
     }
-
-
-  for (int i=0;i<count_pro;i++)  {
-    printf("%s\n",T_product[i].Name);
-  }
+  /*for (int i=0;i<count_pro;i++)  {
+    printf("%s\n",T_product[i].Name);*/
+  //}
 }
 ///////////////////////////////////////////////////////////////////////////////////////////
 void Pro_Qty_3(){
-    int i;
     float price_TTC;
         for(i=0;i<count_pro;i++){
           if(T_product[i].Quantity <3){
             if(T_product[i].Quantity==0){
                 continue;
             }else{
-                printf("##--Product Number \n",i);
-                printf("Name : %s \n",T_product[i].Name);
-                printf("Quantity : %d \n",T_product[i].Quantity);
-                printf("Price : %.2f$ \n",T_product[i].Price);
                 price_TTC=T_product[i].Price + 0.15*T_product[i].Price;
-                printf("Price(TTC) : %.2f$ \n",price_TTC);
-                printf("------------------------------------\n");
+                get_Data(i,price_TTC);
             }
           }
         }
 }
 void Income(){
    int i;
+   printf("count_sold %d",count_sold);
    for(i=0;i<count_sold;i++){
     printf("Name: %s \n",T_sold[i].Name);
     printf("Sold: %d \n",T_sold[i].sold);
-    printf("Price: %f \n",T_sold[i].price);
-    printf("H:M:S %d%d%d \n",T_sold[i].h,T_sold[i].m,T_sold[i].s);
+    printf("Price: %.2f$ \n",T_sold[i].price);
+    printf("Date : %d/%d/%d \n",T_sold[i].day,T_sold[i].month,T_sold[i].year);
     printf("------------------------------------------------\n");
   }
 }
 
 void Statistique(){
-  int i;
+  float  min, max;
   float total=0;
   int qty_total=0;
-  float moyen=1;
+  float moyen;
 
   for(i=0;i<count_sold;i++){
     total+=T_sold[i].price*T_sold[i].sold;
@@ -269,7 +274,6 @@ void Statistique(){
   moyen = total / qty_total;
   printf("moyen of the day is: %.2f\n", moyen);
 
-  float  min, max;
   min = T_sold[0].price;
   for(i=1;i<count_sold;i++){
     if(T_sold[i].price<min) min = T_sold[i].price;
@@ -335,4 +339,3 @@ int main(){
 
  return 0;
 }
-
